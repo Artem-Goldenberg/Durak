9711edc48c0ccf648b6f20d122717b73cc1aa0d8
Artem Goldenberg <st087953@student.spbu.ru>
2024-07-28 00:22:26 +0300

    working durak

--- primitives.py
+++ primitives.py
@@ -1,13 +1,19 @@
 from dataclasses import dataclass
 from enum import Enum, auto
+from functools import total_ordering
 
+@total_ordering
 class Suit(Enum):
     Heart = 0
     Cross = 1
     Spade = 2
     Diamond = 3
+    
+    def __lt__(self, other: 'Suit') -> bool:
+        return self.value < other.value
 
-class Value(Enum):
+@total_ordering
+class CardValue(Enum):
     Six = auto()
     Seven = auto()
     Eight = auto()
@@ -18,11 +24,34 @@ class Value(Enum):
     King = auto()
     Ace = auto()
 
-@dataclass
-class Card:
-    suit: Suit
-    value: Value
+    def __lt__(self, other: 'CardValue') -> bool:
+        return self.value < other.value
 
-class Turn(Enum):
+@dataclass(order=True, eq=True, frozen=True)
+class Card:
+    value: CardValue
+    suit: Suit
+
+    def __repr__(self):
+        return f"{self.value.name}({self.suit.name})"
+
+    def __str__(self) -> str:
+        return f"{self.value.name}({self.suit.name})"
+
+class Switch(Enum):
     First = 1
     Second = 2
+
+    def toggled(self) -> 'Switch':
+        match self:
+            case Switch.First:
+                return Switch.Second
+            case Switch.Second:
+                return Switch.First
+        assert False
+
+class GameResult(Enum):
+    Win = 1
+    Draw = 0
+    Loss = -1
+    Unfinished = -2
